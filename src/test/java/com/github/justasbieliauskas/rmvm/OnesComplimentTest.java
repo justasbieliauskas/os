fca308dac193ef3ccf1bd894a614cdfae6412e87
Justas Bieliauskas <justyvas0@gmail.com>
2018-03-20 18:09:48 +0200

    NOT command.

---
+++ src/test/java/com/github/justasbieliauskas/rmvm/OnesComplimentTest.java
@@ -0,0 +1,22 @@
+package com.github.justasbieliauskas.rmvm;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Tests NOT command.
+ */
+public class OnesComplimentTest
+{
+    @Test
+    public void value() throws Exception {
+        Register A = new Register();
+        WordAssignment not = new WordAssignment(
+            A,
+            new OnesCompliment(() -> 60)
+        );
+        not.execute();
+        assertEquals(A.value(), -61);
+    }
+}
