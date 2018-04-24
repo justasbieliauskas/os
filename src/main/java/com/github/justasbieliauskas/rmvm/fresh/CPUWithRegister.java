b252b3592a863f59834a61298ef575f908abdaaf
Justas Bieliauskas <justyvas0@gmail.com>
2018-04-24 13:26:12 +0300

    Create CPUWithRegister.
    Chaining object to create/update cpu registers.

---
+++ src/main/java/com/github/justasbieliauskas/rmvm/fresh/CPUWithRegister.java
@@ -0,0 +1,43 @@
+package com.github.justasbieliauskas.rmvm.fresh;
+
+import com.github.justasbieliauskas.rmvm.data.Word;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Processor with new register value.
+ *
+ * TODO: document constructors.
+ *
+ * @author Justas Bieliauskas
+ */
+public class CPUWithRegister implements CPU
+{
+    private final CPU processor;
+
+    private final String name;
+
+    private final Word word;
+
+    public CPUWithRegister(Map<String, Integer> registers, String name, Word word) {
+        this(() -> registers, name, word);
+    }
+
+    public CPUWithRegister(String name, Word word) {
+        this(() -> new HashMap<>(), name, word);
+    }
+
+    public CPUWithRegister(CPU processor, String name, Word word) {
+        this.processor = processor;
+        this.name = name;
+        this.word = word;
+    }
+
+    @Override
+    public Map<String, Integer> toMap() {
+        Map<String, Integer> registers = this.processor.toMap();
+        registers.put(this.name, this.word.toInt());
+        return registers;
+    }
+}
