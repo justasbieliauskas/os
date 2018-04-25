8d9f597c08ef415b15cd2275b849cc3d8305ce95
Justas Bieliauskas <justyvas0@gmail.com>
2018-04-25 22:43:54 +0300

    Create COMP instruction.

---
+++ src/main/java/com/github/justasbieliauskas/rmvm/cpu/instruction/CPUAfterCOMP.java
@@ -0,0 +1,55 @@
+package com.github.justasbieliauskas.rmvm.cpu.instruction;
+
+import com.github.justasbieliauskas.rmvm.cpu.CPU;
+import com.github.justasbieliauskas.rmvm.cpu.CPUWithRegister;
+import com.github.justasbieliauskas.rmvm.data.CPURegister;
+import com.github.justasbieliauskas.rmvm.data.WordWithFlag;
+
+import java.util.Map;
+
+/**
+ * Processor after COMP instruction.
+ *
+ * @author Justas Bieliauskas
+ */
+public class CPUAfterCOMP implements CPU
+{
+    private final CPUWithRegister processor;
+
+    /**
+     * @param processor processor
+     */
+    public CPUAfterCOMP(CPU processor) {
+        this(
+            processor,
+            new CPURegister(processor, "A"),
+            new CPURegister(processor, "B"),
+            new CPURegister(processor, "ST")
+        );
+    }
+
+    private CPUAfterCOMP(
+        CPU processor,
+        CPURegister a,
+        CPURegister b,
+        CPURegister status
+    ) {
+        this.processor = new CPUWithRegister(
+            processor,
+            new WordWithFlag(
+                new WordWithFlag(
+                    status,
+                    'Z',
+                    () -> a.toInt() == b.toInt()
+                ),
+                'C',
+                () -> a.toInt() < b.toInt()
+            )
+        );
+    }
+
+    @Override
+    public Map<String, Integer> toMap() {
+        return this.processor.toMap();
+    }
+}
---
+++ src/test/java/com/github/justasbieliauskas/rmvm/cpu/instruction/CPUAfterCOMPTest.java
@@ -0,0 +1,101 @@
+package com.github.justasbieliauskas.rmvm.cpu.instruction;
+
+import com.github.justasbieliauskas.rmvm.WordEquality;
+import com.github.justasbieliauskas.rmvm.cpu.CPUWithRegister;
+import com.github.justasbieliauskas.rmvm.data.CPURegister;
+import com.github.justasbieliauskas.rmvm.data.WordWithFlag;
+import org.junit.Test;
+
+/**
+ * Test for {@link CPUAfterCOMP}.
+ *
+ * @author Justas Bieliauskas
+ * @author Aidas Kvedaravičius
+ * @author Linas Jurgelėnas
+ */
+public class CPUAfterCOMPTest
+{
+    @Test
+    public void firstEqualsSecond() {
+        new WordEquality(
+            new CPURegister(
+                new CPUAfterCOMP(
+                    new CPUWithRegister(
+                        new CPUWithRegister(
+                            new CPUWithRegister("A", 5),
+                            "B",
+                            5
+                        ),
+                        new WordWithFlag(
+                            new WordWithFlag('Z', false),
+                            'C',
+                            true
+                        )
+                    )
+                ),
+                "ST"
+            ),
+            new WordWithFlag(
+                new WordWithFlag('Z', true),
+                'C',
+                false
+            )
+        ).test();
+    }
+
+    @Test
+    public void firstIsGreaterThanSecond() {
+        new WordEquality(
+            new CPURegister(
+                new CPUAfterCOMP(
+                    new CPUWithRegister(
+                        new CPUWithRegister(
+                            new CPUWithRegister("A", 10),
+                            "B",
+                            5
+                        ),
+                        new WordWithFlag(
+                            new WordWithFlag('Z', true),
+                            'C',
+                            true
+                        )
+                    )
+                ),
+                "ST"
+            ),
+            new WordWithFlag(
+                new WordWithFlag('Z', false),
+                'C',
+                false
+            )
+        ).test();
+    }
+
+    @Test
+    public void firstIsLessThanSecond() {
+        new WordEquality(
+            new CPURegister(
+                new CPUAfterCOMP(
+                    new CPUWithRegister(
+                        new CPUWithRegister(
+                            new CPUWithRegister("A", 5),
+                            "B",
+                            10
+                        ),
+                        new WordWithFlag(
+                            new WordWithFlag('Z', true),
+                            'C',
+                            false
+                        )
+                    )
+                ),
+                "ST"
+            ),
+            new WordWithFlag(
+                new WordWithFlag('Z', false),
+                'C',
+                true
+            )
+        ).test();
+    }
+}
