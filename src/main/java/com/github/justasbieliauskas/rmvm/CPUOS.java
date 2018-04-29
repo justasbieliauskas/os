e5c8655e927f7b2da9658eb5262510a85d7ebbb9
Justas Bieliauskas <justyvas0@gmail.com>
2018-04-29 21:03:30 +0300

    Delete CPUAfterCommand.
    New processor will be new'ed in execute() method.

--- src/main/java/com/github/justasbieliauskas/rmvm/CPUOS.java
+++ src/main/java/com/github/justasbieliauskas/rmvm/CPUOS.java
@@ -1,36 +1,29 @@
 package com.github.justasbieliauskas.rmvm;
 
-import com.github.justasbieliauskas.rmvm.cpu.CPUAfterCommand;
 import com.github.justasbieliauskas.rmvm.cpu.MutableCPU;
 
 /**
  * Operating system with processors.
  *
- * TODO: test
- *
  * @author Justas Bieliauskas
  */
-public class DefaultOS implements OS
+public class CPUOS implements OS
 {
     private final MutableCPU processor;
 
-    private final CPUAfterCommand newProcessor;
-
     /**
      * @param processor processor
-     * @param newProcessor processor for commands
      */
-    public DefaultOS(MutableCPU processor, CPUAfterCommand newProcessor) {
+    public CPUOS(MutableCPU processor) {
         this.processor = processor;
-        this.newProcessor = newProcessor;
     }
 
     @Override
     public void execute(String command) throws Exception {
         try {
-            this.processor.update(
-                this.newProcessor.with(this.processor, command).toMap()
-            );
+//            this.processor.update(
+//                this.newProcessor.with(this.processor, command).toMap()
+//            );
         } catch (Exception e) {
             throw new Exception(
                 "Failed to execute command \"" + command + "\".",
