55d05d3abccddc0513cb369f234e4fcc75dc85f9
Justas Bieliauskas <justyvas0@gmail.com>
2018-05-03 09:52:28 +0300

    Create MemoryOfWords.

---
+++ src/test/java/com/github/justasbieliauskas/rmvm/data/MemoryOfWordsTest.java
@@ -0,0 +1,31 @@
+package com.github.justasbieliauskas.rmvm.data;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Test for {@link MemoryOfWords}.
+ *
+ * @author Justas Bieliauskas
+ */
+public class MemoryOfWordsTest
+{
+    @Test
+    public void extractsByte() {
+        assertEquals(
+            2,
+            new MemoryOfWords(77, 566).byteAt(5)
+        );
+    }
+
+    @Test
+    public void modifiesByte() {
+        MemoryOfWords memory = new MemoryOfWords(4, 130, 8903);
+        memory.write((byte) 53, 9);
+        assertEquals((byte) 199, memory.byteAt(8));
+        assertEquals((byte) 53, memory.byteAt(9));
+        assertEquals(0, memory.byteAt(10));
+        assertEquals(0, memory.byteAt(11));
+    }
+}
